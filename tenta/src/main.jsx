00e1483abbef27be97ae5b1d88c03460c79f9d01
maxmaxmax <mrivero1992@gmail.com>
2025-10-06 00:21:29 -0300

    chakraui main

--- tenta/src/main.jsx
+++ tenta/src/main.jsx
@@ -1,6 +1,18 @@
+// INCREMENT: main.jsx ChakraProvider Wrap
+// Type: UI Migration
+// Scope: Wrap root App in ChakraProvider
+// Mode: Candidate (safe preview)
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// Chakra UI
+import { ChakraProvider } from "@chakra-ui/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
